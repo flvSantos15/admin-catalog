11b8c781e79c9f47aa9f1a3595635e28b771522c
flvsantos15 <flvsantos300@gmail.com>
2025-05-01 13:06:48 -0300

    chore: add notification pattern

--- src/category/domain/category-fake.builder.ts
+++ src/category/domain/category-fake.builder.ts
@@ -1,6 +1,6 @@
 import { Chance } from "chance";
-import { Category } from "./category.entity";
 import { Uuid } from "../../shared/domain/value-objects/uuid.vo";
+import { Category } from "./category.entity";
 
 type PropOrFactory<T> = T | ((index: number) => T);
 
@@ -84,7 +84,7 @@ export class CategoryFakeBuilder<TBuild = any> {
             created_at: this.callFactory(this._created_at, index),
           }),
         });
-        //category.validate();
+        category.validate();
         return category;
       });
     return this.countObjs === 1 ? (categories[0] as any) : categories;
