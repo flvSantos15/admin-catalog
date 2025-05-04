045f6c27d1a32ccaa1c3a240d002ef5ebf2263ea
flvsantos15 <flvsantos300@gmail.com>
2025-05-04 20:03:27 -0300

    chore: add category controller test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
+import { CategoryModel } from '@core/category/infra/db/sequelize/category.model';
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [CategoriesModule],
+  imports: [
+    CategoriesModule,
+    SequelizeModule.forRoot({
+      dialect: 'sqlite' as any,
+      host: ':memory:',
+      logging: false,
+      models: [CategoryModel]
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/categories/categories.controller.spec.ts
+++ src/categories/categories.controller.spec.ts
@@ -1,14 +1,31 @@
+import { CategorySequelizeRepository } from '@core/category/infra/db/sequelize/category-sequelize.repository';
+import { CategoryModel } from '@core/category/infra/db/sequelize/category.model';
+import { getModelToken, SequelizeModule } from '@nestjs/sequelize';
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoriesController } from './categories.controller';
-import { CategoriesService } from './categories.service';
 
 describe('CategoriesController', () => {
   let controller: CategoriesController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        SequelizeModule.forRoot({
+          dialect: 'sqlite',
+          host: ':memory:',
+          logging: false,
+          models: [CategoryModel]
+        }),
+        SequelizeModule.forFeature([CategoryModel])
+      ],
       controllers: [CategoriesController],
-      providers: [CategoriesService],
+      providers: [
+        {
+          provide: CategorySequelizeRepository,
+          useFactory: (categoryModel: typeof CategoryModel) => new CategorySequelizeRepository(categoryModel),
+          inject: [getModelToken(CategoryModel)]
+        }
+      ]
     }).compile();
 
     controller = module.get<CategoriesController>(CategoriesController);
