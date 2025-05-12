9c53c534db84c124ac278e2e6a734d147cdbf19f
flvsantos15 <flvsantos300@gmail.com>
2025-05-12 13:46:24 -0300

    chore: use provider in category controller

--- src/nest-modules/categories-module/categories.controller.ts
+++ src/nest-modules/categories-module/categories.controller.ts
@@ -1,33 +1,63 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { CreateCategoryUseCase } from '@core/category/application/use-cases/create-category/create-category.use-case';
+import { DeleteCategoryUseCase } from '@core/category/application/use-cases/delete-category/delete-category.use-case';
+import { GetCategoryUseCase } from '@core/category/application/use-cases/get-category/get-category.use-case';
+import { ListCategoriesUseCase } from '@core/category/application/use-cases/list-category/list-categories.use-case';
+import { UpdateCategoryUseCase } from '@core/category/application/use-cases/update-category/update-category.use-case';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Inject,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 
 @Controller('categories')
 export class CategoriesController {
-  constructor() {}
+  @Inject(CreateCategoryUseCase)
+  private createUseCase: CreateCategoryUseCase;
+
+  @Inject(UpdateCategoryUseCase)
+  private updateUseCase: UpdateCategoryUseCase;
+
+  @Inject()
+  private deleteUseCase: DeleteCategoryUseCase;
+
+  @Inject()
+  private listUseCase: ListCategoriesUseCase;
+
+  @Inject()
+  private getUseCase: GetCategoryUseCase;
 
   @Post()
   create(@Body() createCategoryDto: CreateCategoryDto) {
-    return
+    return;
   }
 
   @Get()
   findAll() {
-    return
+    return;
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return
+    return;
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCategoryDto: UpdateCategoryDto) {
-    return
+  update(
+    @Param('id') id: string,
+    @Body() updateCategoryDto: UpdateCategoryDto,
+  ) {
+    return;
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return
+    return;
   }
 }
